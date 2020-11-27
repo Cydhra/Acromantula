28123368a0af415b9673e8a6102ac603a5c4dd71
Cydhra <ubezl@student.kit.edu>
2020-11-27 19:45:26 +0100

    red test case testing the import server. It crashes because of AbstractMethodError, probably is the API incompatible with modern kotlin?

---
+++ acromantula_backend/src/test/kotlin/net/cydhra/acromantula/rpc/ImportServerTest.kt
@@ -0,0 +1,36 @@
+package net.cydhra.acromantula.rpc
+
+import kotlinx.coroutines.runBlocking
+import net.cydhra.acromantula.ImportMessage
+import net.cydhra.acromantula.bus.EventBroker
+import net.cydhra.acromantula.bus.events.ApplicationStartupEvent
+import net.cydhra.acromantula.commands.CommandDispatcherService
+import net.cydhra.acromantula.rpc.services.ImportServer
+import net.cydhra.acromantula.rpc.services.ImportService
+import net.cydhra.acromantula.rpc.services.asGrpcServer
+import net.cydhra.acromantula.workspace.WorkspaceService
+import org.junit.jupiter.api.Test
+
+class ImportServerTest {
+
+    @Test
+    fun testImport() {
+        runBlocking {
+            EventBroker.registerService(EventBroker)
+            EventBroker.registerService(WorkspaceService)
+            EventBroker.registerService(CommandDispatcherService)
+            EventBroker.fireEvent(ApplicationStartupEvent())
+
+            ImportServer().asGrpcServer(18080) {
+                ImportService.forAddress("localhost", 18080, channelOptions = { usePlaintext() }).use { client ->
+                    client.import(
+                        ImportMessage.newBuilder()
+                            .setDirectoryId(-1)
+                            .setFileUrl(this.javaClass.getResource("testfile.txt").toExternalForm())
+                            .build()
+                    )
+                }
+            }
+        }
+    }
+}
